79eb8c58af6d95f8886d1fa308643e69baf62169
joeychun <joey.s.chun@gmail.com>
2023-02-13 17:01:44 +0900

    added more markers, added route line, added popups

--- map.js
+++ map.js
@@ -2,28 +2,152 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZ3BlciIsImEiOiJjbGUxamo3Y2cwbGxuNDFwYWJqc2RzZ
 
 var geojson = {
   "type": "FeatureCollection",
-  "features": [{
-    "type": "Feature",
-    "geometry": {
-      "type": "Point",
-      "coordinates": [-114.62122816809983, 35.876317610355]
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.19736878608802, 38.83166468952135]
+      },
+      "properties": {
+        "title": "Chris's Hometown",
+        "description": "Chris was born and raised in Annandale, a suburb of D.C. His Father was a high ranking engineer for NASA, then became successful as an entrepreneur. Family conflicts were frequent, as Chris and his father Walt had conflicting views on materialism and the upper-class lifestyle. The father-son relationship further worsens as Chris witnesses Walt's adultery."
+      }
     },
-    "properties": {
-      "title": "Where It Starts.",
-      "description": "Chris's hitchhiking journey into the wild starts here, at Lake Mead, a place in the Detrital Valley. After his Datsun was damaged by a flood, he \"resumed his odyssey on foot.\""
-    }
-  },
-  {
-    "type": "Feature",
-    "geometry": {
-      "type": "Point",
-      "coordinates": [-122.414, 37.776]
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-84.32163951733328, 33.79777758177902]
+      },
+      "properties": {
+        "title": "Emory University",
+        "description": "Chris attended Emory for four years—and he did not enjoy it. He hated that his parents were controlling him to attend an academically prestigious university, so that he can acquire a high-paying job. Believing that \"wealth was shameful, corrupting, inherently evil,\" Chris finds his life in Emory pointless (Krakauer). After his graduation, \"he donated the balence of his bank account to OXFAM, loaded up his car, and vanished from their lives\" (Krakauer)."
+      }
     },
-    "properties": {
-      "title": "Mapbox",
-      "description": "San Francisco, California"
-    }
-  }
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-114.62122816809983, 35.876317610355]
+      },
+      "properties": {
+        "title": "Where It Starts.",
+        "description": "Chris's hitchhiking journey into the wild starts here, at Lake Mead, a place in the Detrital Valley. After his Datsun was damaged by a flood, he \"resumed his odyssey on foot.\" At this point, he adopts a new identity: Alexander Supertramp."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-124.06323840365764, 41.286522130827194]
+      },
+      "properties": {
+        "title": "Chris meets Jan and Bob Burres",
+        "description": "Chris meets Jan and Bob Burres couple in the itinerant community. For the next few months, he continues to live in Jan's trailer where he helps out her bookselling business. <br><br><i>It's been a while since I've been able to connect with people. People no longer judge me around here. They treat me like one of them.</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-112.32771358298041, 48.63366277922529]
+      },
+      "properties": {
+        "title": "Chris meets Wayne Westerberg",
+        "description": "<img style=\"width: 220px;\" src=\"pictures/westerberg.jpeg\"></img> Chris meets Westerberg, who quickly becomes his best friend after letting him sleep overnight in his trailer and later gives him a job. Westerberg is one of the few people that Chris opens up to and confides in. <br><br><i>\"Wayne was the closest thing [I] had found to a father in a long time\" (Krakauer).</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.71564188961703, 44.17013394324656]
+      },
+      "properties": {
+        "title": "Chris temporarily works for Westerberg",
+        "description": "Westerberg suggests Chris a job on one of his grain elevators in South Dakota. Chris impresses Westerberg, as he works hard yet refuses any pay. This scene shows Chris's anti-materialistic philosophy. <br><br><i>Money was not my concern—I just seeked fulfillment through labor. After all, \"you should own nothing except what you can carry on your back at a dead run\" (Krakauer).</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-118.25110598982337, 34.07022913677974]
+      },
+      "properties": {
+        "title": "Chris feels uncomfortable surrounded by people",
+        "description": "Chris was arrested for a day for posessing a firearm on the border. Afterwards heads to Los Angeles \"to get an ID and a job\"—after a long time in the wild (Krakauer). <br><br><i>It \"feels extremely uncomfortable in society now.\" I \"must return to road immediately\" (Krakauer)."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-115.95623517063609, 33.29939578016000]
+      },
+      "properties": {
+        "title": "Chris meets Ronald Franz",
+        "description": "<img style=\"width: 220px;\" src=\"pictures/franz.jpeg\"></img> (Image from \"Into the Wild\" film) <br><br> Chris arrives in Salton City, where he meets Ronald Franz. Franz is another true friend that got Chris to open up to him—Chris talks about his life stories and his theories about life and society. On their last moments, Franz asks Chris if he can adopt him as his grandson, but he dodges answering the question. <br><br><i>…but spending my entire life with him, as his grandson—that wasn't my end goal. I need to keep adventuring.\
+        I need to \"[avade] the impending threat of human intimacy or friendship\" (Krakauer). </i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-108.55107290070937, 39.06617773829494]
+      },
+      "properties": {
+        "title": "Farewell to Franz",
+        "description": "After that final adoption talk, \"Franz left McCandless on the shoulder of Interstate 70 outside <b>Grand Junction</b> and returned to southern California\" (Krakauer)."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.21564188961703, 44.17013394324656]
+      },
+      "properties": {
+        "title": "Chris revisits Westerberg, for the last time",
+        "description": "Chris revisits Westerberg's Carthage, to see Westerberg and his then colleagues for the last time. \"On the morning of April 15, everybody gathered at the elevator to see McCandless off\" (Krakauer). From this point, Chris heads off to Canada and enters a stage of even further solitude, refusing to deeply bond with new people he encounters. <br><br><i>It was time to say goodbye. I will now fully commit myself to nature.</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-126.10633891133796, 59.41516420581774]
+      },
+      "properties": {
+        "title": "Chris catches a ride while heading to Alaska",
+        "description": "Gaylord Stuckey, a trucker, hitchhikes Chris and gets him to Whitehorse, Alaska. At this point, Chris almost completely rejects social interaction; nevertheless, he \"didn’t come out and say too much at first, …but by the end he kingd of let his guard down,\" according to Stuckey. <br><br><i>I've been thinking—maybe it is impossible to live in complete isolation. 'Cause wow, it feels nice to finally have someone to talk to about my personal struggles, after a long time in the wild.</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-147.71441120780042, 64.85160087129377]
+      },
+      "properties": {
+        "title": "Chris gets hitchhiked by Jim Gallien",
+        "description": "Chris meets Jim Gallien, who gives him a ride to Mt. McKinley. Gallien, after noting that \"he wasn’t carrying anywhere near as much food and gear as you’d expect a guy to be carrying for that kind of trip,\" discourages Chris from entering the wild (Krakauer). But Chris, unfazed, ignores him. This scene shows Chris's firm belief in self-reliance. <br><br><i>Jim seemed super worried and asked if I needed any help—really unnecessary. I want to—I need to do things on my own.</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-149.78013312763173, 63.870811806460495]
+      },
+      "properties": {
+        "title": "Chris's Death",
+        "description": "In August 1992, Christopher McCandless was discovered dead in an abandoned bus—due to starvation. Here, he finally realizes that sometimes, seeking help from others is crucial—but it was simply too late for him to change anything. <br><br>Chris's Last Recorded Note:<br><i>\"S.O.S. I need your help. I am injured, \
+        near death, and too weak to hike out of here. I am all alone, this is NO JOKE. In the name of God, please remain to save me. I am out collecting berries close by and shall return this evening. Thank you, Chris McCandless. August?\" (Krakauer)<i/>"
+      }
+    },
   ]
 };
 
@@ -31,17 +155,25 @@ const startingPoint = new mapboxgl.LngLat(-99.93911857048194, 39.83455236598961)
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v12', // style URL
+  projection: 'mercator',
   center: startingPoint, // starting position [lng, lat]
-  zoom: 3.5,
+  zoom: 4,
 });
 
 // add markers to map
 geojson.features.forEach(function (marker, i) {
-
+  if (marker.geometry.type !== "Point") return;
   // create a HTML element for each feature
   var el = document.createElement('div');
   el.className = 'marker';
-  el.innerHTML = '<i class="fa fa-map-marker" style="font-size:33px;color:#00008b;text-shadow:1px 1px 2px #000000;"></i> <div style="color: white; transform: translate(6px, -20px);"><b>' + (i + 1) + '</b></div>'
+  if (i + 1 <= 2) {
+    el.innerHTML = '<i class="fa fa-map-marker" style="opacity:60%;font-size:33px;color:#ff2e2e;text-shadow:1px 1px 2px #000000;"></i> <div style="color: white; transform: translate(6px, -20px);"><b>' + (i + 1) + '</b></div>'
+  }
+  else if (i + 1 < 10) {
+    el.innerHTML = '<i class="fa fa-map-marker" style="opacity:60%;font-size:33px;color:#00008b;text-shadow:1px 1px 2px #000000;"></i> <div style="color: white; transform: translate(6px, -20px);"><b>' + (i + 1) + '</b></div>'
+  } else {
+    el.innerHTML = '<i class="fa fa-map-marker" style="opacity:60%;font-size:33px;color:#00008b;text-shadow:1px 1px 2px #000000;"></i> <div style="color: white; transform: translate(3px, -20px);"><b>' + (i + 1) + '</b></div>'
+  }
 
   // make a marker for each feature and add it to the map
   new mapboxgl.Marker({
@@ -55,15 +187,93 @@ geojson.features.forEach(function (marker, i) {
     .addTo(map);
 });
 
-// var popup1 = new mapboxgl.Popup()
-//   .setText('Description')
+map.on('load', () => {
+  map.addSource('route', {
+    'type': 'geojson',
+    'data': {
+      'type': 'Feature',
+      'properties': {},
+      'geometry': {
+        'type': 'LineString',
+        'coordinates': [
+          [-77.19736878608802, 38.83166468952135],
+          [-84.32163951733328, 33.79777758177902],
+          [-114.62122816809983, 35.876317610355],
+          [-124.06323840365764, 41.286522130827194],
+          [-112.32771358298041, 48.63366277922529],
+          [-97.71564188961703, 44.17013394324656],
+          [-118.25110598982337, 34.07022913677974],
+          [-115.95623517063609, 33.29939578016000],
+          [-108.55107290070937, 39.06617773829494],
+          [-97.21564188961703, 44.17013394324656],
+          [-126.10633891133796, 59.41516420581774],
+          [-147.71441120780042, 64.85160087129377],
+          [-149.78013312763173, 63.870811806460495],
+        ]
+      }
+    }
+  });
+  map.addLayer({
+    'id': 'route',
+    'type': 'line',
+    'source': 'route',
+    'layout': {
+      'line-join': 'round',
+      'line-cap': 'round'
+    },
+    'paint': {
+      'line-color': '#1338be',
+      'line-width': 1
+    }
+  });
+  map.addSource('points', {
+    'type': 'geojson',
+    'data': {
+      'type': geojson.type,
+      'features': geojson.features.slice(0, 2)
+    }
+  });
+  map.addLayer({
+    'id': 'circle',
+    'type': 'circle',
+    'source': 'points',
+    'paint': {
+      'circle-color': '#ff2e2e',
+      'circle-radius': 8,
+      'circle-stroke-width': 2,
+      'circle-stroke-color': '#ffffff'
+    }
+  });
+  map.addSource('points2', {
+    'type': 'geojson',
+    'data': {
+      'type': geojson.type,
+      'features': geojson.features.slice(2, 100)
+    }
+  });
+  map.addLayer({
+    'id': 'circle2',
+    'type': 'circle',
+    'source': 'points2',
+    'paint': {
+      'circle-color': '#4264fb',
+      'circle-radius': 8,
+      'circle-stroke-width': 2,
+      'circle-stroke-color': '#ffffff'
+    }
+  });
+});
 
-// let marker1 = new mapboxgl
-//   .Marker({
-//     color: "#000000",
-//     scale: 0.5
-//   })
-//   .setLngLat(new mapboxgl.LngLat(-114.62122816809983, 35.876317610355))
-//   .addTo(map)
-//   .setPopup(popup1);
+map.on('click', 'circle', function (e) {
+  map.jumpTo({
+    center: e.features[0].geometry.coordinates,
+    zoom: 5.5,
+  });
+})
 
+map.on('click', 'circle2', function (e) {
+  map.jumpTo({
+    center: e.features[0].geometry.coordinates,
+    zoom: 5.5,
+  });
+})
